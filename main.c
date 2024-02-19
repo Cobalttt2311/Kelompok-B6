b83defd410555bf51f9de325c60dd2be07df6486
radja restu arsita <radja.restu.tif23@Polban.ac.id>
2024-02-19 11:56:53 +0700

    nambah jojo

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 printf("hai lagi");
-<<<<<<< HEAD
 printf("hai ");
-=======
-printf("Apa aja");
->>>>>>> 6f0339391085fbe5fb7bab594703439978970ee2
 
 printf("Haloo manies");
+printf("jojo")
