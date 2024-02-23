209ec4256b65d8102f0ccb35fa644ddcb4239ce2
Nalendra Praja Bredtyopati Yudo <nalendra.praja.tif23@polban.ac.id>
2024-02-23 09:19:08 +0700

    Cobain ubah teks di dalam

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 printf("hai lagi");
-<<<<<<< HEAD
-printf("Apa aja");
-=======
 printf("hai ");
 
 printf("Haloo manies");
 printf("jojo")
->>>>>>> b83defd410555bf51f9de325c60dd2be07df6486
+
+printf("Halo guys Semangat ya semuanya");
