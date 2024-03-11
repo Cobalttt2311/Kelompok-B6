055776268ef1826d22d9ab68ba4b6c2aa01d4113
daffalghi <152256525+daffalghi@users.noreply.github.com>
2024-03-12 06:57:22 +0700

    Update aesmain.h

--- aesmain.h
+++ aesmain.h
@@ -4,4 +4,4 @@
 // aes_main,  enkripsi AES pada state menggunakan kunci yang diperluas untuk putaran yang ditentukan
 void aes_main(unsigned char *state, unsigned char *expandedKey, int nbrRounds);
 
-#endif /* AESMAIN_H */
+#endif 
