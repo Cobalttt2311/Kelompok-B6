a58ba71eb3494b2e3fa77ae586464878a8812556
Alanna Tanisya Anwar <alanna.tanisya.tif23@polban.ac.id>
2024-03-15 13:59:58 +0700

    penambahan sintaks pada library shiftrows

--- enum.h
+++ enum.h
@@ -1,3 +1,7 @@
+#define enum_h
+#include <stdio.h>
+#include <stdlib.h>
+
 // enum KeySize, digunakan untuk merepresentasikan ukuran kunci
 enum keySize;
 //enum errorCode, untuk penanda kesalahan
--- shiftrows.h
+++ shiftrows.h
@@ -1,37 +1,6 @@
-// Fungsi untuk melakukan operasi ShiftRows
-void shiftRows(unsigned char state[NB][NB]) {
-    shiftRow2(state);
-    shiftRow3(state);
-    shiftRow4(state);
-}
+#define shiftrows_h
+#include <stdio.h>
+#include <stdlib.h>
 
-// Fungsi untuk melakukan pergeseran baris pada baris kedua
-void shiftRow2(unsigned char state[NB][NB]) {
-    unsigned char temp;
-    temp = state[1][0];
-    state[1][0] = state[1][1];
-    state[1][1] = state[1][2];
-    state[1][2] = state[1][3];
-    state[1][3] = temp;
-}
-
-// Fungsi untuk melakukan pergeseran baris pada baris ketiga
-void shiftRow3(unsigned char state[NB][NB]) {
-    unsigned char temp;
-    temp = state[2][0];
-    state[2][0] = state[2][2];
-    state[2][2] = temp;
-    temp = state[2][1];
-    state[2][1] = state[2][3];
-    state[2][3] = temp;
-}
-
-// Fungsi untuk melakukan pergeseran baris pada baris keempat
-void shiftRow4(unsigned char state[NB][NB]) {
-    unsigned char temp;
-    temp = state[3][0];
-    state[3][0] = state[3][1];
-    state[3][1] = state[3][2];
-    state[3][2] = state[3][3];
-    state[3][3] = temp;
-}
+// shiftRows, Terapkan transformasi ShiftRows pada state
+void shiftRows(unsigned char *state);
