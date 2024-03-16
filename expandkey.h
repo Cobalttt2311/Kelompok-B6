2b744665f032b73e6b238e80cfb8fde411ccaccb
Erina Dwi Yanti <erina.dwi.tif23@polban.ac.id>
2024-03-16 19:07:11 +0700

    mengubah expandkey.h

--- expandkey.h
+++ expandkey.h
@@ -1,5 +1,10 @@
+#ifnedef expandkey_h
+#define expandkey_h
+
 //Procedure pembangkit_kunci AES Encrypt 128 Bit
 void pembangkit_kunci(char *word, int iteration);
 
 // Implementasi procedure AES round
 void aes_round(char *state, char *roundKey);
+
+#endif
