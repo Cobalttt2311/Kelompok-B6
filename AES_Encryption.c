74eed7002a209b1791805099e734ca5c76ebc39a
alannatnsyaa <158986485+alannatnsyaa@users.noreply.github.com>
2024-03-17 23:11:12 +0700

    Update AES_Encryption.c

--- AES_Encryption.c
+++ AES_Encryption.c
@@ -86,20 +86,18 @@ void subBytes(int baris, int kolom, unsigned char state[baris][kolom]) {
 
 
 // shiftRows, Terapkan transformasi ShiftRows pada state
-void shiftRows(unsigned char *state) {
-    int i, j;
-    unsigned char tmp;
-
-    for (i = 0; i < 4; i++) {
-        // Menggeser baris ke kiri sesuai dengan nomor barisnya
-        for (j = 0; j < i; j++) {
-            tmp = state[i * 4]; // Simpan byte pertama
-            state[i * 4] = state[i * 4 + 1]; // Geser byte ke-2 ke byte pertama
-            state[i * 4 + 1] = state[i * 4 + 2]; // Geser byte ke-3 ke byte ke-2
-            state[i * 4 + 2] = state[i * 4 + 3]; // Geser byte ke-4 ke byte ke-3
-            state[i * 4 + 3] = tmp; // Pindahkan byte pertama ke byte ke-4
-        }
+void shiftRows(unsigned char state[4][4]) {
+  int i, j;
+  unsigned char tmp;
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < i; j++) {
+      tmp = state[i][0];
+      state[i][0] = state[i][1];
+      state[i][1] = state[i][2];
+      state[i][2] = state[i][3];
+      state[i][3] = tmp;
     }
+  }
 }
 
 // addRoundKey, Terapkan AddRoundKey pada state
