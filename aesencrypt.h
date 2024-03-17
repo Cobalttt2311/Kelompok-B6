09cdea88333ec7f6317fed0170ff16de5ca487c7
Cobalttt2311 <152157640+Cobalttt2311@users.noreply.github.com>
2024-03-17 23:32:57 +0700

    Update aesencrypt.h

--- aesencrypt.h
+++ aesencrypt.h
@@ -1,6 +1,8 @@
 #ifnedef AESENCRYPT_H
 #define AESENCRYPT_H
 
+//Punya Nalendra
+
 // Implementasi : encrypt
 char aes_encrypt(unsigned char *input, unsigned char *output, unsigned char *key, enum keySize size);
 
