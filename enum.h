f14097b096b1b97340061308b0490e70ecfb9981
alannatnsyaa <158986485+alannatnsyaa@users.noreply.github.com>
2024-03-17 13:20:13 +0700

    Update enum.h

--- enum.h
+++ enum.h
@@ -1,5 +1,5 @@
-#IFNDEF enum_h
-#DEFINE enum_h
+#ifndef ENUM_H
+#define ENUM_H
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,4 +8,4 @@ enum keySize;
 //enum errorCode, untuk penanda kesalahan
 enum errorCode;
 
-#ENDIF
+#endif
