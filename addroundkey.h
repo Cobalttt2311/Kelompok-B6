538fd3e50b10429c68555faa7fcf2bcd4e1a5671
daffalghi <152256525+daffalghi@users.noreply.github.com>
2024-04-15 08:25:47 +0700

    Update addroundkey.h

--- addroundkey.h
+++ addroundkey.h
@@ -2,7 +2,7 @@
 #define ADDROUNDKEY_H
 
 // createRoundKey: Membuat kunci pertama, dan juga membuat kunci putaran dari expanded key.
-void createRoundKey(unsigned char expandedKey[4][4], unsigned char roundKey[4][4])
+void createRoundKey(unsigned char expandedKey[4][4], unsigned char roundKey[4][4]);
 
 // addRoundKey: Setelah kunci putaran dibuat oleh createRoundKey Menambahkan kunci putaran ke dalam state. , langkah ini digunakan untuk menambahkan kunci putaran tersebut ke state. 
 void addRoundKey(unsigned char state[4][4], unsigned char roundKey[4][4]);
