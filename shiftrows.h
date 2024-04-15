f943688ef472b7e5968204f8d240cee02434f3d4
daffalghi <152256525+daffalghi@users.noreply.github.com>
2024-04-15 08:14:26 +0700

    Update shiftrows.h

--- shiftrows.h
+++ shiftrows.h
@@ -1,7 +1,5 @@
 #ifndef SHIFTROWS_H
 #define SHIFTROWS_H
-#include <stdio.h>
-#include <stdlib.h>
 
 // shiftRows, Terapkan transformasi ShiftRows pada state
 void shiftRows(unsigned char state[4][4]);
