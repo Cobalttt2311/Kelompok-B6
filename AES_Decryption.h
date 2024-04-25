e0fea16cf9b43afe4d9cf4bd6c19c54b306375b6
daffalghi <152256525+daffalghi@users.noreply.github.com>
2024-04-25 15:54:24 +0700

    Update AES_Decryption.h

--- AES_Decryption.h
+++ AES_Decryption.h
@@ -1,5 +1,7 @@
 #ifndef DECRYPTION_H
 #define DECRYPTION_H
 
+void aes_invRound(unsigned char state[4][4], unsigned char roundKey[4][4]); //inverse tahapan  di round
+char aes_decrypt(unsigned char *input, unsigned char *output, unsigned char *key, enum keySize size); //proses decrypt 
 
 #endif
