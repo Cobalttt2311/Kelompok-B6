c9f7875a6cc5572a513344007203221d66b2df47
erinadwy <158986567+erinadwy@users.noreply.github.com>
2024-04-29 04:28:07 +0700

    Update AES_Decryption.c
    update the expanded key

--- AES_Decryption.c
+++ AES_Decryption.c
@@ -92,6 +92,27 @@ char aes_decrypt(unsigned char *input, unsigned char *output, unsigned char *key
     break;
   }
 
+  // Set the block values
+  for (i = 0; i < 4; i++)
+  {
+    for (j = 0; j < 4; j++)
+      block[(i + (j * 4))] = input[(i * 4) + j];
+  }
+
+  // Expand key menjadi 176 bytes key
+  expandKey(expandedKey[0], key, size, 16 * (nbrRounds + 1));
+
+  // Decrypt the block using the expandedKey
+  // Pass the address of block (which acts as 2D array)
+  aes_invMain(block, expandedKey[0], nbrRounds); 
+
+  // Unmap the block again into the output
+  for (i = 0; i < 4; i++)
+  {
+    for (j = 0; j < 4; j++)
+      output[(i * 4) + j] = block[(i + (j * 4))];
+  }
+
   return SUCCESS;
 }
 
