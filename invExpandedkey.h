3d260bb52da143ab3ec785ea473ce837e1611028
daffalghi <152256525+daffalghi@users.noreply.github.com>
2024-04-29 08:54:40 +0700

    SEMICOLON ADUHAI

--- invExpandedkey.h
+++ invExpandedkey.h
@@ -1,6 +1,6 @@
 #ifndef INVEXPANDEDKEY_H
 #define INVEXPANDEDKEY_H
 
-  char aes_decrypt(unsigned char *input, unsigned char *output, unsigned char *key, enum keySize size)
+  char aes_decrypt(unsigned char *input, unsigned char *output, unsigned char *key, enum keySize size;
 
 #endif 
