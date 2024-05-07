d3690ac01fff6cb6f0d864794d13c5fa3b421fb7
erinadwy <158986567+erinadwy@users.noreply.github.com>
2024-05-07 14:25:52 +0700

    Update LSB_Decrypt.h

--- LSB_Decrypt.h
+++ LSB_Decrypt.h
@@ -1,6 +1,7 @@
 #ifndef LSB_DECRYPTION_H
 #define LSB_DECRYPTION_H
 
+// Fungsi untuk mendekripsi pesan yang disembunyikan dalam gambar menggunakan metode LSB
 void decrypt_message(const unsigned char* image_data, int image_size, int message_size, char* decrypted_message);
 
 #endif
