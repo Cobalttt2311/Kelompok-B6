afb2b47d0800cbb70b7423ec9f0f930b6c02529a
Radja-Restu-A <158986576+Radja-Restu-A@users.noreply.github.com>
2024-05-13 07:33:54 +0700

    add modul hapus node tipuan

--- algoritmaLL.c
+++ algoritmaLL.c
@@ -16,3 +16,19 @@ void sisipkantipuan(address first) {
         next(temp) = tipuan;
     }
 }
+
+
+void hapustipuan(address first) {
+    int j;
+    address kiri;
+    address tipuan;
+    for (j = 1; j <= 6; j++) {
+        kiri = nodesebelumpalsu(fibonaci(j), first);
+        tipuan = next(kiri);
+        next(kiri) = next(next(kiri));
+        if (next(kiri) != Nil) {
+            prev(next(kiri)) = kiri;
+        }
+        free(tipuan);
+    }
+}
