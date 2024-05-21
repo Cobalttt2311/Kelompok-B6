2f70c28f2af8821ae97a3e3ffdaecc97cedc0dee
daffalghi <152256525+daffalghi@users.noreply.github.com>
2024-05-21 11:36:18 +0700

    Update AES_Decryption.c

--- AES_Decryption.c
+++ AES_Decryption.c
@@ -33,7 +33,6 @@ char inverseSbox[16][16] = {
     {0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d}  // F
 };
 
-
 void invsubBytes(int ukuran, unsigned char state[ukuran][ukuran]) {
   int i, j;
   for (i = 0; i < ukuran; i++) {
@@ -45,77 +44,21 @@ void invsubBytes(int ukuran, unsigned char state[ukuran][ukuran]) {
   }
 }
 
-void aes_invRound(unsigned char state[4][4], unsigned char roundKey[4][4])
-{
-  invShiftRows(state);
-  invSubBytes(4,state);
-  addRoundKey(state, roundKey);
-  invMixColumns(state);
-}
-
-char aes_decrypt(unsigned char *input, unsigned char *output, unsigned char *key, enum keySize size)
-{
-  // the number of rounds
-  int nbrRounds;
-
-  // the expanded key
-  unsigned char expandedKey[16][16]; // 15 rounds maximum
-
-  // the 128 bit block to decode
-  unsigned char block[BLOCK_SIZE];
-
-  int i, j;
-
-  // set the number of rounds
-  switch (size)
-  {
-  case SIZE_16:
-    nbrRounds = 10;
-    break;
-  default:
-    return ERROR_AES_UNKNOWN_KEYSIZE;
-    break;
-  }
-
-  // Set the block values
-  for (i = 0; i < 4; i++)
-  {
-    for (j = 0; j < 4; j++)
-      block[(i + (j * 4))] = input[(i * 4) + j];
-  }
-
-  // Expand the key into a 176 bytes key
-  expandKey(expandedKey[0], key, size, 16 * (nbrRounds + 1));
-
-  // Decrypt the block using the expandedKey
-  // Pass the address of block (which acts as 2D array)
-  aes_invMain(block, expandedKey[0], nbrRounds); 
-
-  // Unmap the block again into the output
-  for (i = 0; i < 4; i++)
-  {
-    for (j = 0; j < 4; j++)
-      output[(i * 4) + j] = block[(i + (j * 4))];
-  }
-
-  return SUCCESS;
-}
-
 void invShiftRows(unsigned char state[4][4]) {
     int i, j, k;
     unsigned char tmp;
 
     for (i = 0; i < 4; i++) {
-        // jumlah pergeseran dengan berdasarkan indeks baris
+        // Number of cyclic shifts for this row (based on row index)
         int shift = i;
 
-        // proses pergeseran ke kanan dengan posisi 'shift'
+        // Perform cyclic shift to the right by `shift` positions
         for (j = 0; j < shift; j++) {
-            tmp = state[i][3];  // menyimpan elemen terakhir
-            for ( k = 2; k >= 0; k--) {  // menggeser elemen ke kanan
+            tmp = state[i][3];  // Store the last element
+            for ( k = 2; k >= 0; k--) {  // Shift elements to the right
                 state[i][k + 1] = state[i][k];
             }
-            state[i][0] = tmp;  // memindahkan elelmen yang disimpan ke awal
+            state[i][0] = tmp;  // Move the stored element to the beginning
         }
     }
 }
