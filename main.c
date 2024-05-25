a4ddff67eb90e19e8282e945b5696d650ccdc7b8
Cobalttt2311 <152157640+Cobalttt2311@users.noreply.github.com>
2024-05-25 11:01:01 +0700

    Update main.c

--- main.c
+++ main.c
@@ -283,6 +283,9 @@ int main()
                 clear_list(&firstDec);
 		printf("=============================================================\n");
 		printf("|                      AES & LSB Kelompok 6                 |\n");
+		printf("=============================================================\n");
+		printf("|                         DEKRIPSI                          |\n");
+		printf("-------------------------------------------------------------\n");
 	        printf("\nMasukkan Cipher Key (16 karakter dalam format HEX):\n");
 		fflush(stdin);
 		for (i = 0; i < 16; i++){
