42cd22030fb7f6955b04687ebea885b9e91b799d
daffalghi <152256525+daffalghi@users.noreply.github.com>
2024-05-25 08:17:37 +0700

    Update LSB_Decryption.h

--- LSB_Decryption.h
+++ LSB_Decryption.h
@@ -2,6 +2,6 @@
 #define LSB_DECRYPTION_H
 
 // Fungsi untuk mendekripsi pesan yang disembunyikan dalam gambar menggunakan metode LSB
-void decrypt_message(const unsigned char* image_data, int image_size, int message_size, char* decrypted_message);
+void decrypt_message(const unsigned char* image_data, int image_size, int message_size, char decrypted_message);
 
 #endif
