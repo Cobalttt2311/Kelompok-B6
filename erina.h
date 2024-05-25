3b2eef02e81007c2d06aab96abe7fb09368f245a
erinadwy <158986567+erinadwy@users.noreply.github.com>
2024-05-25 17:25:50 +0700

    Update comment erina.h

--- erina.h
+++ erina.h
@@ -1,7 +1,9 @@
 #ifndef ERINA_H
 #define ERINA_H
 
-void insert(address *first, infotype isi); 
+//Prosedur untuk menyisipkan elemen baru(chiper text) ke dalam linked list
+void insert(address *first, infotype isi);
+//Fungsi untuk memindahkan data dari node pertama linked list ke array
 infotype pindahkearray(address *first);
 
 #endif
